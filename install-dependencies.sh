3a831d4045db39413e96548820b684459bc429bc
Gabriel Ziegler <gabrielziegler3@gmail.com>
2019-10-16 20:53:40 -0300

    Separate scripts in data and server dependencies installation
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,14 +1,4 @@
 #! /bin/bash
-
-# Install docker
-sudo snap install docker
-
-# Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo docker-compose --version
-
 mkdir inverse-cooking/data 
 
 wget -nc https://dl.fbaipublicfiles.com/inversecooking/ingr_vocab.pkl \
